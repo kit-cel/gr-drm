fa686c619237de1de69566d801fd70d4039d8e56
Felix Wunsch <uncnr@student.kit.edu>
2012-09-13 12:26:39 +0200

    added script to substitute the hard coded paths in the hier blocks with the correct home folders. .grc_gnuradio is assumed to be found at $HOME/.grc_gnuradio.

---
+++ misc/set_grc_path.sh
@@ -0,0 +1,3 @@
+sed "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_64qam_sm_vbvb.py.xml
+sed "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_4qam_vbvb.py.xml
+sed "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_16qam_vbvb.py.xml
