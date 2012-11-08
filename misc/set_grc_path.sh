924df3e26196dcbd476f18a865bfe7a966fae268
Felix Wunsch <uncnr@student.kit.edu>
2012-11-08 20:49:27 +0100

    changed set_grc_path.sh to reflect the renaming of some files and added bachelor thesis as documentation for the project.

--- misc/set_grc_path.sh
+++ misc/set_grc_path.sh
@@ -1,3 +1,3 @@
-sed -i "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_64qam_sm_vbvb.py.xml
-sed -i "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_4qam_vbvb.py.xml
-sed -i "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_16qam_vbvb.py.xml
+sed -i "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_64qam_sm_vbvc.py.xml
+sed -i "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_4qam_vbvc.py.xml
+sed -i "s#/home/felixwunsch#$HOME#" ../../hier_blocks/drm_mlc_16qam_vbvc.py.xml
