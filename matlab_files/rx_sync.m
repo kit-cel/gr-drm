ec2a544dab8cdb7c68911f6823c5080043de509c
Felix Wunsch <uncnr@student.kit.edu>
2012-12-10 16:33:34 +0100

    added channel to the simulation (AWGN, multipath, doppler, frequency offset)

--- matlab_files/rx_sync.m
+++ matlab_files/rx_sync.m
@@ -16,31 +16,81 @@ s = s./sqrt(p_s);
 
 % TODO: Kanal simulieren (Daempfung, Phasenverschiebung, Frequenzverschiebung,
 % Doppler, Mehrwege...)
+nsamp_sym = fs * 0.4; % Samples pro Symbol (400 ms)
+delta_t = 1/fs; % Abtastzeit
+delta_f = fs/nsamp_sym; % Abstand der Frequenzpunkte der FFT
+n_mp = 5; % Anzahl Mehrwege
 
-% AWGN addieren
-r = awgn(s, 100, 'measured');
+n_runs = 50; % Anzahl Test-Durchlaeufe
+n_failed = 0; % Anzahl fehlerhafter Synchronisationsvorgaenge
+for n = 1 : n_runs
+    t = 0:delta_t:delta_t*(length(s)-1);
+    f_off = randi([-80, 80], [n_mp 1]); % zufaellige Frequenzverschiebung (Vielfache von delta_f)
+    t_off = randi([0 200], [n_mp 1]); % zufaellige Zeitverschiebung (Samples, Vielfache von delta_t)
+    a = randn([n_mp 1]) + 1i * randn([n_mp 1]);  % komplexe Kanalkoeffizienten (Rayleigh)
+    %abs(a);
 
-%% Frequenzsynchronisation
+    r = zeros(length(s), 1); % Speicher vorbelegen
+    for i = 1 : n_mp % Zeit-, phasen- und frequenzverschobene, gedaempfte Kopie vom Sendesignal addieren
+        r = r + [zeros(t_off(i), 1); a(i) * s(1:end-t_off(i)) .* exp(1i*2*pi*delta_f*f_off(i)*t(1:end-t_off(i))')];  
+    end
 
-% Schaetzung durch FFT. FFT-Laenge entsprechend der maximal moeglichen
-% Untertraegeranzahl.
+    % AWGN addieren
+    r = awgn(r, 20, 'measured');
 
-nfft = 2^13; % 460 Traeger maximal moeglich (RM A, SO 5)
-nsamp_sym = 48e3 * 0.4; % 48e3*400ms
-r_w = r(1:nsamp_sym); % gefenstertes Empfangssignal (evtl. Fensterfkt. verwenden)
-%f = 0:fs/nfft:fs - fs/nfft; % Frequenzachse
-R_w = abs(fft(r_w, nfft));
-plot(R_w); %xlabel('Frequenz in Hz'); ylabel('|R(f)|');
+    %% Frequenzsynchronisation DRM30
 
-% Extrahieren der 3 groessten Spektrallinien
-freq_pil_index = [0 0 0];
-for i = 1 : 3
-    freq_pil_index(i) = find(R_w == max(R_w));
-    R_w(freq_pil_index(i)) = 0;
+    % Ansatz aus DREAM: Schaetzung der PSD aus FFT ueber ein Symbol, dann
+    % Korrelation mit den bekannten Positionen der Frequenzpiloten
+
+    % Energieschaetzung des Eingangssignals
+
+    psd_est = fftshift(abs(fft(r(1000:1000+nsamp_sym), nsamp_sym).^2));
+    psd_est = psd_est/max(psd_est); % Normierung auf Maximum
+    f_axis = [0 : fs/nsamp_sym : fs - fs/nsamp_sym];
+
+    % Korrelation mit bekannten Pilotpositionen
+    freq_pil_index = round([750/delta_f 2250/delta_f 3000/delta_f]);
+    freq_pil = zeros(1, ceil(3000/delta_f));
+    freq_pil(freq_pil_index) = 1; % entsprechende Bins auf 1 setzen
+    [corr, index] = xcorr(psd_est, freq_pil); % Kreuzkorrelation berechnen und fftshift
+    dc_index = index(find(corr == max(abs(corr)))); % DC has to be at the maximum of the cross-correlation function
+
+    % Ergebnis ueberpruefen - Der Algorithmus sollte den Pfad mit dem
+    % groessten abs(a) gefunden haben
+
+    max_a = find(a == max(a));
+    f_max = f_off(max_a) + 1;
+
+    if dc_index == f_max
+        disp('freq sync OK');
+    else
+        disp('freq sync FAILED');
+        n_failed = n_failed + 1;
+        
+        abs(a)
+        f_off
+        dc_index    
+    end
 end
-freq_pil_index = sort(freq_pil_index);
-freq_pil_f = freq_pil_index*fs/nfft;
-freq_pil_f(3) - freq_pil_f(2)
-freq_pil_f(2) - freq_pil_f(1)
+
+success_rate = 1 - n_failed/n_runs % Erfolgsquote
+
+
+% Ergebnisse plotten
+plot_res = 0;
+
+if plot_res == 1
+    figure(); 
+
+    subplot(2,1,1); 
+    plot(f_axis, psd_est); 
+    xlabel('Frequenz in Hz'); ylabel('|R(f)|^2'); title('PSD Schaetzung');
+
+
+    subplot(2,1,2);
+    plot(index, abs(corr));
+    xlabel('Index'); ylabel('xcorr'); title('Korrelation der Frequenzpiloten');
+end
 
 
