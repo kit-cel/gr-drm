227ea29efb6c16a808ebb0fe954b1892f08f979f
Felix Wunsch <uncnr@student.kit.edu>
2012-12-18 19:31:32 +0100

    some more guard correlation experiments.

--- matlab_files/rx_timesync_acq.m
+++ matlab_files/rx_timesync_acq.m
@@ -6,7 +6,9 @@
 % die Tracking Frequency Range um den Dezimationsfaktor kleiner als bei
 % einem Frequenzbereichsalgorithmus.
 
-% Guard Time Correlation ermoeglicht auch eine Detektion des RMj
+% Guard Time Correlation ermoeglicht auch eine Detektion des RM
+% - korrekte Annahme der Symboldauer u. Laenge des Guardintervalls fuehrt
+%   zu deutlichem Peak.
 
 % Signal auf interessanten Bereich filtern (z. B. 10 KHz)
 
@@ -20,22 +22,25 @@ s = complex_baseband; % reines Basisbandsignal ohne Kanaleinfluss
 p_s = var(s);
 s = s./sqrt(p_s);
 
-Ts = .4/15; % OFDM Symboldauer RM B
+% Ts = .4/15; % OFDM Symboldauer RM B
+Ts = .733/15; % Falscher OFDM Symboltakt
 nsamp_Ts = ceil(fs*Ts); % Anzahl Samples pro OFDM Symbol
 Tg = 5.33/1000; % CP Dauer RM B
+% Tg = 5.2/1000; % falsche CP Dauer
 nsamp_Tg = ceil(fs*Tg); % Anzahl Samples im CP
 
 % Guard Time Correlation
 
-avg_sym = 10; % Anzahl der Symbole, ueber die gemittelt wird
-decim_factor = 5; % Dezimationsfaktor fuer die Korrelation
+avg_sym = 1; % Anzahl der Symbole, ueber die gemittelt wird
+nsym = 1; % Anzahl der konsekutiven Symbole, die in die Korrelation einbezogen werden
+decim_factor = 1; % Dezimationsfaktor fuer die Korrelation
 lag = 300 % Verschiebung vom Anfang des Symbols in Samples (unabh. von decim_factor)
 
-s = awgn(s, 5, 'measured'); % AWGN addieren
+s = awgn(s, 15, 'measured'); % AWGN addieren
 
-corrvec = zeros(1, ceil(nsamp_Ts/decim_factor)); % Vektor fuer die Korrelationswerte
+corrvec = zeros(1, nsym * ceil(nsamp_Ts/decim_factor)); % Vektor fuer die Korrelationswerte
 corrlen = length(corrvec);
-for start = 0 : decim_factor : avg_sym*nsamp_Ts-1
+for start = 0 : decim_factor : nsym*avg_sym*nsamp_Ts-1
     corrvec(mod(ceil(start/decim_factor),length(corrvec))+1) = ... % Korrelation berechnen und zum vorhandenen Werte addieren
         corrvec(mod(ceil(start/decim_factor),corrlen)+1) + ...
         corr(s(start+lag+1:start+lag+nsamp_Tg+1), s(start+lag+1+nsamp_Ts-nsamp_Tg:start+lag+nsamp_Ts+1));
